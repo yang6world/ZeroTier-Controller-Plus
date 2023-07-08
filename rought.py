eb027e37b49d7152ffd26ca6317b921b51c7cc01
Yang Zhirui <136310248+yang6world@users.noreply.github.com>
2023-07-08 19:01:05 +0800

    未完工

---
+++ rought.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, Response
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def main_route():
+    data = request.get_json()
+    if "'Sage'" in data or "Claude+" in data or "Claude_instant" in data:
+        response = requests.post('https://poeapi.yserver.top', json=data)
+    else:
+        response = requests.post('https://api.openai-sb.com', json=data)
+
+    # 构造一个 Flask 的 Response 对象，将上游的状态码、头部信息和内容全部返回给原请求者
+    return Response(response.content, response.status_code, response.headers.items())
+
+if __name__ == '__main__':
+    app.run(port=9091)
