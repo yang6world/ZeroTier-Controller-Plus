527e9bf2e8efe4468e2e4202adb82a635b82958e
Yang <yzr@yserver.top>
2023-12-23 23:28:51 +0800

    prepare

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 from fastapi import FastAPI
+from api.config import config
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+config = config()
