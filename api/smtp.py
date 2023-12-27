72193fe4657056f3257d628edccd8640c2eb51c1
Yang <yzr@yserver.top>
2023-12-27 21:08:13 +0800

    prepare

--- api/smtp.py
+++ api/smtp.py
@@ -13,19 +13,28 @@ class Email:
         self.mail_pass = config.SMTP_PASSWORD  # 口令
         self.sender = config.SMTP_SENDER
 
-    def sing_in(self, receivers, subject, message):
-        message = MIMEText(message, 'plain', 'utf-8')
+    def send_registration_email(self, receivers, subject, user_name):
+        # 构建注册确认的邮件内容
+        message_content = f"亲爱的 {user_name},\n\n感谢您注册我们的服务。请点击下面的链接来激活您的账户:\n{registration}"
+
+        # 设置邮件格式
+        message = MIMEText(message_content, 'plain', 'utf-8')
         message['From'] = Header(self.sender, 'utf-8')
         message['To'] = Header(receivers, 'utf-8')
         message['Subject'] = Header(subject, 'utf-8')
+
         try:
+            # 连接到SMTP服务器
             s = smtplib.SMTP_SSL(self.mail_host, 587)
             s.starttls()
             s.ehlo()
             s.login(self.mail_user, self.mail_pass)
+
+            # 发送邮件
             s.sendmail(self.sender, receivers, message.as_string())
             s.close()
             return True
         except smtplib.SMTPException as e:
             print(e)
             return False
+
